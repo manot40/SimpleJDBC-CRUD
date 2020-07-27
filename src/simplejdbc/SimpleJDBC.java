3f3c8cda966fc4e3a88d9fc44a0b2995a376808f
manot40 <kevinsandiho290100@gmail.com>
2020-07-27 20:41:35 +0700

    Update SimpleJDBC.java

--- src/simplejdbc/SimpleJDBC.java
+++ src/simplejdbc/SimpleJDBC.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package simplejdbc;
 
 import java.sql.Connection;
@@ -6,9 +11,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.Scanner;
-
 /**
- * @NIM 2201860355
+ *
  * @author Kevin Sandiho
  */
 public class SimpleJDBC {
@@ -51,13 +55,12 @@ public class SimpleJDBC {
             stt.close();
         } catch(SQLException ex) {
             System.out.println("Tidak dapat terhubung ke DBMS, error message:");
-            System.err.println(ex.getMessage());
+            //System.err.println(ex.getMessage());
         }
         System.out.println("+-------+--------------------------------+----------------------+------------+------+");
     }
     
-    private final static void clearConsole() throws InterruptedException {
-        Thread.sleep(1500);
+    private final static void clearConsole(){
         try {
             final String os = System.getProperty("os.name");
             if (os.contains("Windows")){
@@ -71,99 +74,110 @@ public class SimpleJDBC {
             System.err.println(ex.getMessage());
         }
     }
-    
-    public static void main(String args[]) throws InterruptedException {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         String title, author;
         float price;
-        int id, qty, pilihan;
+        int id, qty;
         
         while(true){
             clearConsole();
             getData();
             System.out.println("\nPilih Salah satu menu dibawah ini :");
-            System.out.println("1.) Penambahan Data");
-            System.out.println("2.) Update Data");
-            System.out.println("3.) Hapus Data");
-            System.out.println("4.) Keluar");
+            System.out.println("1) Penambahan Data");
+            System.out.println("2) Update Data");
+            System.out.println("3) Hapus Data");
+            System.out.println("4) Keluar");
             System.out.print("\nMasukan angka menu pilihan kamu : ");
-            pilihan = input.nextInt();
-            
-            switch(pilihan){
-                case 1:
-                    System.out.println("Masukan detail data baru");
-                    System.out.print("Title: ");
-                    input.nextLine();
-                    title = input.nextLine();
-                    System.out.print("Author: ");
-                    author = input.nextLine();
-                    System.out.print("Price: ");
-                    price = input.nextFloat();
-                    System.out.print("Qty: ");
-                    qty = input.nextInt();
+            try{
+                var pilihan = Integer.parseInt(input.nextLine());
+                switch(pilihan){
+                    case 1:
+                        System.out.println("Masukan detail data baru");
+                        System.out.print("Title: ");
+                        input.nextLine();
+                        title = input.nextLine();
+                        System.out.print("Author: ");
+                        author = input.nextLine();
+                        System.out.print("Price: ");
+                        price = input.nextFloat();
+                        System.out.print("Qty: ");
+                        qty = input.nextInt();
                     
-                    try{
-                        Connection conn=(Connection)ConnectDB.connectDB();
-                        Statement stt=conn.createStatement();
-                        stt.executeQuery("INSERT INTO books(title,author,price,qty) VALUES('"+title+"','"+author+"','"+price+"','"+qty+"')");
-                        stt.close();
-                        System.out.println("TAMBAH DATA BERHASIL!");
-                    } catch(SQLException ex) {
-                        System.out.println("Tidak dapat terhubung ke DBMS, error message:");
-                        System.err.println(ex.getMessage());
-                    }
-                    break;
+                        try{
+                            Connection conn=(Connection)ConnectDB.connectDB();
+                            Statement stt=conn.createStatement();
+                            stt.executeQuery("INSERT INTO books(title,author,price,qty) VALUES('"+title+"','"+author+"','"+price+"','"+qty+"')");
+                            stt.close();
+                            System.out.println("TAMBAH DATA BERHASIL!");
+                        } catch(SQLException ex) {
+                            System.out.println("Tidak dapat terhubung ke DBMS, error message:");
+                            System.err.println(ex.getMessage());
+                        }
+                        Thread.sleep(1500);
+                        break;
+                        
+                    case 2: 
+                        System.out.print("Masukan id data yang akan diubah: ");
+                        input.nextLine();
+                        id = Integer.parseInt(input.nextLine());
+                        System.out.println("\nMasukan detail perubahan");
+                        System.out.print("Title: ");
+                        title = input.nextLine();
+                        System.out.print("Author: ");
+                        author = input.nextLine();
+                        System.out.print("Price: ");
+                        price = input.nextFloat();
+                        System.out.print("Qty: ");
+                        qty = input.nextInt();
+                        
+                        try{
+                            Connection conn=(Connection)ConnectDB.connectDB();
+                            Statement stt=conn.createStatement();
+                            stt.executeQuery("UPDATE books SET title='"+title+"',author='"+author+"',price='"+price+"',qty='"+qty+"' WHERE id='"+id+"'");
+                            stt.close();
+                            System.out.println("UPDATE BERHASIL!");
+                        } catch(SQLException ex) {
+                            System.out.println("Tidak dapat terhubung ke DBMS, error message:");
+                            System.err.println(ex.getMessage());
+                        }
+                        Thread.sleep(1500);
+                        break;
+                    
+                    case 3:
+                        System.out.print("Masukan id data yang akan dihapus: ");
+                        id = input.nextInt();
+                        
+                        try{    
+                            Connection conn=(Connection)ConnectDB.connectDB();
+                            Statement stt=conn.createStatement();
+                            stt.executeQuery("DELETE FROM books WHERE id='"+id+"'");
+                            stt.close();
+                            System.out.println("HAPUS DATA BERHASIL!");
+                        } catch(SQLException ex) {
+                            System.out.println("Tidak dapat terhubung ke DBMS, error message:");
+                            System.err.println(ex.getMessage());
+                        }
+                        Thread.sleep(1500);
+                        break;
+                    
+                    case 4:
+                        System.out.println("Exiting Program...");
+                        System.exit(0);
                 
-                case 2:
-                    System.out.print("Masukan id data yang akan diubah: ");
-                    input.nextLine();
-                    id = Integer.parseInt(input.nextLine());
-                    System.out.println("\nMasukan detail perubahan");
-                    System.out.print("Title: ");
-                    title = input.nextLine();
-                    System.out.print("Author: ");
-                    author = input.nextLine();
-                    System.out.print("Price: ");
-                    price = input.nextFloat();
-                    System.out.print("Qty: ");
-                    qty = input.nextInt();
-                    
-                    try{
-                        Connection conn=(Connection)ConnectDB.connectDB();
-                        Statement stt=conn.createStatement();
-                        stt.executeQuery("UPDATE books SET title='"+title+"',author='"+author+"',price='"+price+"',qty='"+qty+"' WHERE id='"+id+"'");
-                        stt.close();
-                        System.out.println("UPDATE BERHASIL!");
-                    } catch(SQLException ex) {
-                        System.out.println("Tidak dapat terhubung ke DBMS, error message:");
-                        System.err.println(ex.getMessage());
-                    }
-                    break;
-                    
-                case 3:
-                    System.out.print("Masukan id data yang akan dihapus: ");
-                    id = input.nextInt();
-                    
-                    try{
-                        Connection conn=(Connection)ConnectDB.connectDB();
-                        Statement stt=conn.createStatement();
-                        stt.executeQuery("DELETE FROM books WHERE id='"+id+"'");
-                        stt.close();
-                        System.out.println("HAPUS DATA BERHASIL!");
-                    } catch(SQLException ex) {
-                        System.out.println("Tidak dapat terhubung ke DBMS, error message:");
-                        System.err.println(ex.getMessage());
-                    }
-                    break;
-                    
-                case 4:
-                    System.out.println("Exiting Program...");
-                    System.exit(0);
+                    default:
+                        System.out.println("Pilihan Invalid!");
+                        Thread.sleep(1500);
+                        break;
+                }
                 
-                default:
-                    System.out.println("Pilihan Invalid!");
-                    break;
+            }catch(NumberFormatException ex){
+                System.out.println("Input Invalid!");
             }
         }
     }
+    
 }
